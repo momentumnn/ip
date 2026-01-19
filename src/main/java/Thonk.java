2c31f39515c9146f1a0f4bfed03a78d2dfb4d15f
Tan Je-Deon <114093710+momentumnn@users.noreply.github.com>
2026-01-19 12:26:00 +0800

    Update name to Thonk

---
+++ src/main/java/Thonk.java
@@ -0,0 +1,13 @@
+public class Thonk {
+    public static void main(String[] args) {
+        String logo ="""
+               _____ _                 _
+              |_   _| |__   ___  _ __ | | __
+                | | | '_ \\ / _ \\| '_ \\| |/ /
+                | | | | | | (_) | | | |   <
+                |_| |_| |_|\\___/|_| |_|_|\\_\\
+              """;
+
+        System.out.println("Hello from\n" + logo + "what u want \n");
+    }
+}
