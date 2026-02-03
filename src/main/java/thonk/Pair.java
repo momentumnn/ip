e1bfd0dcef1c163d63f095e0ed253ae70cc57043
Tan Je-Deon <114093710+momentumnn@users.noreply.github.com>
2026-02-03 15:34:36 +0800

    Add javadoc for Pair

--- src/main/java/thonk/Pair.java
+++ src/main/java/thonk/Pair.java
@@ -1,18 +1,35 @@
 package thonk;
 
+/**
+ * A convenience class to represent name-value pairs.
+ * @param <T> Pair 1
+ * @param <U> Pair 2
+ */
 public class Pair<T, U> {
     private final T t;
     private final U u;
 
+    /**
+     * Creates a new pair
+     * @param t key
+     * @param u value
+     */
     public Pair(T t, U u) {
         this.t = t;
         this.u = u;
     }
-
+    /**
+     * Gets the key for this pair.
+     * @return key
+     */
     public T t() {
         return this.t;
     }
 
+    /**
+     * Gets the value for this pair
+     * @return value
+     */
     public U u() {
         return this.u;
     }
