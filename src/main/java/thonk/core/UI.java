334f59b281a9ec23ae02a8105c6d9ee9116fe881
Tan Je-Deon <114093710+momentumnn@users.noreply.github.com>
2026-02-03 15:32:14 +0800

    Add javadoc for UI

--- src/main/java/thonk/core/UI.java
+++ src/main/java/thonk/core/UI.java
@@ -7,24 +7,44 @@ import java.util.Scanner;
 
 import thonk.Task;
 
+/**
+ * The class UI controls the system.in and system.out of Thonk.
+ */
 public class UI {
     private final Scanner in;
     private final PrintStream out;
 
+    /**
+     * Creates a new class of UI
+     */
     public UI() {
         this(System.in, System.out);
     }
 
-    public UI(InputStream in, PrintStream out) {
+    private UI(InputStream in, PrintStream out) {
         this.in = new Scanner(in);
         this.out = out;
     }
+
+    /**
+     * Returns the next line received from system.in
+     * @return String of next line
+     */
     public String getNextLine() {
         return in.nextLine();
     }
+
+    /**
+     * Prints the text loaded.
+     * @param text text to be printed.
+     */
     public void print(String text) {
         out.println(text);
     }
+
+    /**
+     * Prints banner
+     */
     public void banner() {
         String divider = "_______________________________\n\n";
         String logo = """
@@ -36,6 +56,11 @@ public class UI {
                 """;
         out.println("Hello from\n" + logo + "what u want \n" + divider);
     }
+
+    /**
+     * Prints the current list of tasks within pastTasks
+     * @param pastTasks ArrayList of tasks
+     */
     public void list(ArrayList<Task> pastTasks) {
         if (pastTasks.isEmpty()) {
             out.println("There is nothing to do.");
@@ -47,16 +72,35 @@ public class UI {
             i++;
         }
     }
+
+    /**
+     * Prints goodbye text
+     */
     public void goodbye() {
         out.println("Good bye");
     }
+
+    /**
+     * Prints default text after adding a task. Also states how many tasks there are.
+     * @param task task that is created
+     * @param size number of tasks in arrayList
+     */
     public void add(Task task, int size) {
         out.println("Noted with thanks, \nadded " + task.getDescription() + " to ur list\nCurrently u have " + size
                 + " of stuff");
     }
+    /**
+     * Prints default text after deleting a task.
+     * @param task task that is deleted
+     */
     public void delete(Task task) {
         out.println("Noted with thanks, \nsay bye bye to " + task.getDescription() + " from ur list");
     }
+
+    /**
+     * Prints default text after marking a task.
+     * @param task task that is marked
+     */
     public void mark(Task task) {
         if (task.getDone()) {
             out.println("ok slay its done\n" + task);
