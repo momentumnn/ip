2de1a23136ecf8c79500c28bf3f85005c7283d5a
Tan Je-Deon <114093710+momentumnn@users.noreply.github.com>
2026-02-03 15:26:32 +0800

    Add javadoc for TaskManager

--- src/main/java/thonk/core/TaskManager.java
+++ src/main/java/thonk/core/TaskManager.java
@@ -6,29 +6,61 @@ import java.util.stream.IntStream;
 import thonk.Storage;
 import thonk.Task;
 
+/**
+ * The class TaskManager stores all tasks and handles the file management between Thonk and the filesystem.
+ */
 public class TaskManager {
     private final Storage storage;
     private final ArrayList<Task> tasks;
 
+    /**
+     * Creates an instance of TaskManager with a path to the file
+     * @param path String path to file
+     */
     public TaskManager(String path) {
         storage = new Storage(path);
         tasks = storage.load();
     }
+
+    /**
+     * Creates an instance of TaskManager with no specific path to file.
+     * Path is decided via Storage class
+     */
     public TaskManager() {
         storage = new Storage();
         tasks = storage.load();
     }
+
+    /**
+     * Returns an ArrayList of Tasks
+     * @return ArrayList<\Task> of tasks
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
+
+    /**
+     * Adds task into the current ArrayList and file
+     * @param task Task to be added
+     */
     public void add(Task task) {
         tasks.add(task);
         storage.save(tasks);
     }
+    /**
+     * Delete task from the current ArrayList and file
+     * @param task Task to be deleted
+     */
     public void delete(Task task) {
         tasks.remove(task);
         storage.save(tasks);
     }
+
+    /**
+     * Marks task as per isDone parameter
+     * @param task Task to be marked
+     * @param isDone boolean value of isDone
+     */
     public void mark(Task task, Boolean isDone) {
         tasks.get(this.getTaskIndex(task)).setDone(isDone);
         storage.save(tasks);
