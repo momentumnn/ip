f89eb01d983470b4af1688a192160d0ae51ed3ee
Tan Je-Deon <114093710+momentumnn@users.noreply.github.com>
2026-02-03 15:22:25 +0800

    Add javadoc for ThonkException and its subclass

--- src/main/java/thonk/IncompleteCommandException.java
+++ src/main/java/thonk/IncompleteCommandException.java
@@ -1,7 +1,11 @@
 package thonk;
 
+/**
+ * The class IncompleteCommandException is thrown when the user inputs a wrong command.
+ * A wrong command is when it does not match the commands provided.
+ */
 public class IncompleteCommandException extends ThonkException {
     public IncompleteCommandException(String message) {
-        super("Incomplete Thonk.Command, try again: " + message);
+        super("Incomplete command, try again: " + message);
     }
 }
--- src/main/java/thonk/ThonkException.java
+++ src/main/java/thonk/ThonkException.java
@@ -1,5 +1,8 @@
 package thonk;
 
+/**
+ * The class ThonkException is thrown when there is an exception that happened within the program itself.
+ */
 public class ThonkException extends RuntimeException {
     public ThonkException(String message) {
         super(message);
